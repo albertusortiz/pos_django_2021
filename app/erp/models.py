2b809cf4d7e96adaba34f3b475901d716d7ad6e2
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2021-04-24 22:14:06 -0500

    Creating relation foreign key and manytomany.

--- app/erp/models.py
+++ app/erp/models.py
@@ -3,7 +3,33 @@ from django.db import models
 from django.utils import timezone
 
 
+class Type(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Nombre')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Tipo'
+        verbose_name_plural = 'Tipos'
+        ordering = ['id']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Nombre')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering = ['id']
+
+
 class Employee(models.Model):
+    categ = models.ManyToManyField(Category)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
     names = models.CharField(max_length=150, verbose_name='Nombres')
     dni = models.CharField(max_length=10, unique=True, verbose_name='DNI')
     date_joined = models.DateField(default=timezone.now, verbose_name='Fecha de registro')
